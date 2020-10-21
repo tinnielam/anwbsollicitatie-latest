0d2b6d2093e2fc0190b205b1d87ac132fa0c4ca7
tinnielam <tinlamngoc@gmail.com>
2020-10-21 18:54:03 +0200

    1 file modified

--- index.tsx
+++ index.tsx
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import "./Verkeersinformatie/Styles/Verkeersinformatie.scss";
-import Jams from './Verkeersinformatie/Jams/VerkeersinformatieJams';
-import Roadworks from './Verkeersinformatie/Radars/VerkeersinformatieRadars';
-import Radars from './Verkeersinformatie/Roadworks/VerkeersinformatieRoadworks';
-import Map from './GoogleMaps/GoogleMaps';
+import Jams from "./Verkeersinformatie/Jams/VerkeersinformatieJams";
+import Roadworks from "./Verkeersinformatie/Radars/VerkeersinformatieRadars";
+import Radars from "./Verkeersinformatie/Roadworks/VerkeersinformatieRoadworks";
+import Map from "./GoogleMaps/GoogleMaps";
 
 render(<Map />, document.getElementById("map"));
 render(<Jams />, document.getElementById("Jams"));
