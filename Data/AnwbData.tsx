6cd54a98993ff476bc8b7739a336e9398d811c7c
tinnielam <tinlamngoc@gmail.com>
2020-10-24 23:12:21 +0200

    1 file modified

--- Data/AnwbData.tsx
+++ Data/AnwbData.tsx
@@ -6,20 +6,4 @@ export default class AnwbData {
       .then(res => res.json())
       .then(res => res.map((data: any) => data));
   }
-
-  async getAnwbDataPolylineJams(): Promise<[]> {
-    return await fetch(
-      `https://anwbtinlam.tinlam.repl.co/verkeersinformatiejams`
-    )
-      .then(res => res.json())
-      .then(res =>
-        res.map((verkeersinformatie: any) =>
-          verkeersinformatie.segments.map(segments =>
-            segments.jams
-              .filter(jams => typeof jams.polyline !== "undefined")
-              .map(locationJams => console.log(locationJams))
-          )
-        )
-      );
-  }
 }
